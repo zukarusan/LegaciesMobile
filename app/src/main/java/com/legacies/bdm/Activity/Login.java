34e24c594f5cd4db93ff2081c8009b3e5c051424
unknown <ceqinq>
2019-10-14 12:53:51 +0700

    add activities and fragments

---
+++ app/src/main/java/com/legacies/bdm/Activity/Login.java
@@ -0,0 +1,16 @@
+package com.legacies.bdm.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.legacies.bdm.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/legacies/bdm/Fragment/Recipient.java
@@ -0,0 +1,32 @@
+package com.legacies.bdm.Fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.legacies.bdm.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Recipient extends Fragment {
+
+
+    public Recipient() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipient, container, false);
+    }
+
+}
