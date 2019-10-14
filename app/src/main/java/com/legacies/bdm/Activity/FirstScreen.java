11816d78a0ef01be7797dd46dc7d9056b6e41397
Nabil Asykar <muhammadnabilasykar34@gmail.com>
2019-10-14 14:53:27 +0700

    added first screen and login activity

---
+++ app/src/main/java/com/legacies/bdm/Activity/FirstScreen.java
@@ -0,0 +1,16 @@
+package com.legacies.bdm.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.legacies.bdm.R;
+
+public class FirstScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_screen);
+    }
+}
